dfe6ff5f4126db6b48a9a5ac83742e22eba470a6
jhenriquevb <waysftw@gmail.com>
2022-12-31 19:51:53 -0300

    beecrowd | 1008

---
+++ exercises/beecrowd1008.py
@@ -0,0 +1,6 @@
+numFuncionario = int(input())
+horasTrabalhadas = int(input())
+valorHora = float(input())
+
+print(f"NUMBER = {numFuncionario}")
+print(f"SALARY = U$ {horasTrabalhadas * valorHora:.2f}")
